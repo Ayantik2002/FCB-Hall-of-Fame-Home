f1db9052ee517b237e07f042cb063a793b4015e8
Ayantik2002 <ayantikbhaumik@gmail.com>
2023-04-07 19:43:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+function App() {
+  // const name=prompt("Enter your name:")
+  return (
+    <main>
+      <h1 className="title">FC BARCELONA: HALL OF FAME</h1>
+      <p className="instructions">
+        Hello Culers, Welcome to the home of the Greats!! A place for
+        celebration, admiration and nostalgia.
+      </p>
+      <button className="btn">
+        <a className="link--text" href="https://ayantik-fcb-hof-hp.netlify.app">
+          Click
+        </a>
+      </button>
+    </main>
+  );
+}
+
+export default App;
